80007781f7b9e83e33d4f3eb62a3ad10ad53f55c
Emmanuel <emmamalenga4@gmail.com>
2025-08-27 14:27:59 +0200

    govhjblnkl;'

--- ShoppingApp/App.js
+++ ShoppingApp/App.js
@@ -1,7 +1,7 @@
-import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
 
@@ -11,8 +11,16 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Products' }} />
-        <Stack.Screen name="Detail" component={DetailScreen} options={{ title: 'Product Details' }} />
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{ title: 'Products' }} 
+        />
+        <Stack.Screen 
+          name="Detail" 
+          component={DetailScreen} 
+          options={{ title: 'Product Details' }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
